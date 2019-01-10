e5315ecaabb9bcdf4abdfd1bcdd5b4cc03a13c4f
liangfapeng <Fredtwins@163.com>
2019-01-10 15:06:46 +0800

    修改生产环境等IP地址

--- static/config/index.js
+++ static/config/index.js
@@ -5,8 +5,8 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://219.132.198.90:8080/mpsp';
-  // window.SITE_CONFIG['baseUrl'] = 'http://10.0.1.184:8080/mpsp';
+  // window.SITE_CONFIG['baseUrl'] = 'http://219.132.198.90:8080/mpsp';
+  window.SITE_CONFIG['baseUrl'] = 'http://10.0.1.184:8080/mpsp';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
